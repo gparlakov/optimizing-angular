fa74551865f92943ec9f8b5564160872a8234e46
Georgi Parlakov <gparlakov@gmail.com>
2019-05-25 21:28:37 +0300

    roll back the ethers and bip39 lazy

--- src/app/personal/personal.component.ts
+++ src/app/personal/personal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as bip39 from 'bip39';
+import * as ethers from 'ethers';
 
 @Component({
   selector: 'app-personal',
@@ -14,14 +16,10 @@ export class PersonalComponent implements OnInit {
   ngOnInit() {}
 
   createWallet() {
-    const bip39Import = import('bip39');
-    const ethersImport = import('ethers');
-    Promise.all([bip39Import, ethersImport]).then(([bip39, ethers]) => {
-      const mnemonic = bip39.generateMnemonic();
-      const wallet = ethers.Wallet.fromMnemonic(mnemonic);
-      this.generatedMnemonic = mnemonic;
-      this.walletAddress = wallet.address;
-      this.walletPrivateKey = wallet.privateKey;
-    });
+    const mnemonic = bip39.generateMnemonic();
+    const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+    this.generatedMnemonic = mnemonic;
+    this.walletAddress = wallet.address;
+    this.walletPrivateKey = wallet.privateKey;
   }
 }
