970ee1213b2819fae5736bea5cd8bad0b0818107
Georgi Parlakov <gparlakov@gmail.com>
2019-05-25 19:50:37 +0300

    lazy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { PersonalComponent } from './personal/personal.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'personal', component: PersonalComponent },
+  { path: 'login', loadChildren: './login/login.module#LoginModule' },
+  { path: 'personal', loadChildren: './personal/personal.module#PersonalModule' },
   { path: '**', component: HomeComponent }
 ];
 
