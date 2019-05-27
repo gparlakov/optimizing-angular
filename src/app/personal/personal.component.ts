19c2687bca586f67d43ac1c32926c0f1615d944b
Georgi Parlakov <gparlakov@gmail.com>
2019-05-27 20:28:17 +0300

    Make the bip load lazy

--- src/app/personal/personal.component.ts
+++ src/app/personal/personal.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import * as bip39 from 'bip39';
-import * as ethers from 'ethers';
+// import * as bip39 from 'bip39';
+// import * as ethers from 'ethers';
 
 @Component({
   selector: 'app-personal',
@@ -16,10 +16,14 @@ export class PersonalComponent implements OnInit {
   ngOnInit() {}
 
   createWallet() {
-    const mnemonic = bip39.generateMnemonic();
-    const wallet = ethers.Wallet.fromMnemonic(mnemonic);
-    this.generatedMnemonic = mnemonic;
-    this.walletAddress = wallet.address;
-    this.walletPrivateKey = wallet.privateKey;
+    const bip39Promise = import(/* webpackChunkName: 'bip39_ethers' */'bip39');
+    const ethersPromise = import(/* webpackChunkName: 'bip39_ethers' */'ethers');
+    Promise.all([bip39Promise, ethersPromise]).then(([bip39, ethers]) => {
+      const mnemonic = bip39.generateMnemonic();
+      const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+      this.generatedMnemonic = mnemonic;
+      this.walletAddress = wallet.address;
+      this.walletPrivateKey = wallet.privateKey;
+    });
   }
 }
